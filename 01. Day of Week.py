a6da62643c4c7176d25715de68a39bd498efbf71
Margarita-Simeonova <margito.sl@gmail.com>
2024-10-16 19:09:41 +0300

    Day of Week
    Day of Week

---
+++ 01. Day of Week.py
@@ -0,0 +1,22 @@
+def day_of_week(numb):
+
+    if numb == 1:
+        return "Monday"
+    elif numb == 2:
+        return "Tuesday"
+    elif numb == 3:
+        return "Wednesday"
+    elif numb == 4:
+        return "Thursday"
+    elif numb == 5:
+        return "Friday"
+    elif numb == 6:
+        return "Saturday"
+    elif numb == 7:
+        return "Sunday"
+    else:
+        return "Error"
+
+
+number = int(input())
+print(day_of_week(number))
